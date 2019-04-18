b5f672a79ded0d248fe297a74a8731c8fa9a915d
Olivier BERNARD <olivier.bernard_ext@softathome.com>
2019-04-18 10:13:45 +0200

    Added docstring and logging to template.

--- templates/skeleton.py
+++ templates/skeleton.py
@@ -2,6 +2,21 @@
 # coding: utf-8
 
 
+"""
+File:
+Author: 
+Email: 
+Github: 
+Description: TODO: Write me!
+"""
+
+
+import logging
+
+
+LOGGER = logging.getLogger(__name__)
+
+
 def main():
     pass
 
